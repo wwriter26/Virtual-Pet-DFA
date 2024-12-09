f4ad6c4a65ae842f2aa3e07cb89b98867a60b847
baxterromero44 <89486675+baxterromero44@users.noreply.github.com>
2024-12-09 12:08:44 -0700

    added sound fx
    added sound fx and created playSound function

--- virtual-pet/src/components/Pet/Pet.js
+++ virtual-pet/src/components/Pet/Pet.js
@@ -5,17 +5,34 @@ class Pet extends DFA {
     super();
     this.hunger = 0;
     this.happiness = 0;
+
+    this.sounds = {
+      feed: new Audio(require('../../assets/images/sounds/feed.mp3')),
+      play: new Audio(require('../../assets/images/sounds/play.mp3')),
+      evolve: new Audio(require('../../assets/images/sounds/evolution.mp3')),
+      train: new Audio(require('../../assets/images/sounds/train.mp3')),
+      hatch: new Audio(require('../../assets/images/sounds/hatch.mp3')),
+    };
+  }
+
+  playSound(action) {
+    if (this.sounds[action]) {
+      this.sounds[action].play();
+    }
   }
 
   hatch() {
     this.transition('hatch');
+    this.playSound('hatch');
   }
+
 
   evolve() {
     if ((this.hunger === 100) && (this.happiness === 100)){
       this.transition('evolve');
       this.happiness = 0
       this.hunger = 0
+      this.playSound('evolve');
     }
   }
 
@@ -23,17 +40,21 @@ class Pet extends DFA {
     this.hunger = Math.min(100, this.hunger + 25);
     this.happiness = Math.min(100, this.happiness + 10);
     this.transition('feed');
+    this.playSound('feed');
   }
 
   play() {
     this.happiness = Math.min(100, this.happiness + 25);
     this.hunger = Math.min(100, this.hunger - 10);
+    this.playSound('play');
   }
 
   train() {
     this.happiness = Math.min(100, this.happiness + 30);
     this.hunger = Math.min(100, this.hunger - 15);
     this.transition('train');
+    this.playSound('train');
+
   }
 
   getCurrentState() {
